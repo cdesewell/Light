8656b8b6e78163095cabfea6784a39bf1d28a069
Chris <chris.sewell@bigavocado.digital>
2019-11-17 12:36:43 +0000

    Initial commit

---
+++ Assets/Scripts/CharacterLight.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLight : MonoBehaviour
+{
+    private Rigidbody2D characterRigidBody;
+    GameObject torch;
+
+    public float rotateSpeed;
+    public float upperViewLimit;
+    public float lowerViewLimit;
+
+    public float rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterRigidBody = GetComponent<Rigidbody2D>();
+        torch = GameObject.Find("CharacterLight");
+
+        torch.transform.eulerAngles = new Vector3
+        {
+            x = 0,
+            y = 0,
+            z = rotation
+        };
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipTorch();
+        PositionTorch();
+        RotateTorch();
+        RestrictTorch();
+    }
+
+    private void PositionTorch()
+    {
+        torch.transform.position = new Vector3
+        {
+            x = characterRigidBody.position.x,
+            y = characterRigidBody.position.y,
+            z = 0f
+        };
+    }
+
+    private void FlipTorch()
+    {
+        var desairedDirection = Input.GetAxis("Horizontal");
+
+        var currentDirection = Mathf.Abs(torch.transform.rotation.eulerAngles.z);
+
+        if (desairedDirection < 0 && (currentDirection < 360 && currentDirection > 180))
+        {
+            var diff = rotation * 2;
+            rotation -= diff;
+        }
+
+        if (desairedDirection > 0 && (currentDirection > 0 && currentDirection < 180))
+        {
+            var diff = rotation * -2;
+            rotation += diff;
+        }
+    }
+
+    private void RotateTorch()
+    {
+        var verticalRotation = Input.GetAxis("Vertical");
+
+        if(rotation < 0)
+        {
+            if (verticalRotation > 0)//Look Down
+            {
+                rotation -= rotateSpeed;
+            }
+            else if (verticalRotation < 0) //Look Up
+            {
+                rotation += rotateSpeed;
+            }
+        }
+        else
+        {
+            if (verticalRotation > 0)//Look Up
+            {
+                rotation += rotateSpeed;
+            }
+            else if (verticalRotation < 0) //Look Down
+            {
+                rotation -= rotateSpeed;
+            }
+        }
+
+        torch.transform.eulerAngles = new Vector3
+        {
+            x = 0,
+            y = 0,
+            z = rotation
+        };
+
+    }
+
+    private void RestrictTorch()
+    {
+        if (rotation < -(upperViewLimit) && rotation < 0)
+        {
+            rotation = -(upperViewLimit);
+        }
+        else if (rotation > -(lowerViewLimit) && rotation < 0)
+        {
+            rotation = -(lowerViewLimit);
+        }
+        else if (rotation < (lowerViewLimit) && rotation > 0)
+        {
+            rotation = (lowerViewLimit);
+        }
+        else if (rotation > (upperViewLimit) && rotation > 0)
+        {
+            rotation = (upperViewLimit);
+        }
+
+        torch.transform.eulerAngles = new Vector3
+        {
+            x = rotateSpeed * 0,
+            y = rotateSpeed * 0,
+            z = rotation
+        };
+    }
+}
+
+public enum CharacterDirectin
+{
+    Left,
+    Right
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float acceleration;
+    public float topSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D characterRigidBody;
+
+    private bool _isTouchingGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Move();
+    }
+
+    private void Jump()
+    {
+        if (_isTouchingGround)
+        {
+            var jumpInput = Input.GetButtonDown("Jump");
+
+            if (jumpInput)
+            {
+                characterRigidBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if (_isTouchingGround)
+        {
+            var horizontalForce = Input.GetAxis("Horizontal");
+
+            var velocity = new Vector2(horizontalForce * Time.deltaTime * acceleration, 0f);
+
+            if ((Mathf.Abs(characterRigidBody.velocity.x) < topSpeed))
+            {
+                characterRigidBody.AddForce(velocity, ForceMode2D.Impulse);
+            }
+            else
+            {
+                characterRigidBody.velocity = new Vector2(topSpeed * Mathf.Sign(velocity.x), characterRigidBody.velocity.y);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            _isTouchingGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            _isTouchingGround = false;
+        }
+    }
+}
